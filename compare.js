4d6bdde6f01ea762e792171f9a1edddf8dce5286
tom8825 <tlparker1988@gmail.com>
2019-08-02 13:06:24 +0100

    added test result read-out and percent difference

--- compare.js
+++ compare.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const PNG = require('pngjs').PNG;
 const pixelmatch = require('pixelmatch');
+let testThreshold = 5;
 
 module.exports = {
     compareScreenShots: async function (image1, image2, config) {
@@ -10,10 +11,19 @@ module.exports = {
         const { width, height } = img1;
         const diff = new PNG({ width, height });
 
-        pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 });
+        let numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 });
 
         fs.writeFileSync('compare-images/' + config[0] + "/" + config[0] + '-diff.png', PNG.sync.write(diff));
 
+        //this does not display % diff correctly
+        let percentDiff = parseFloat(numDiffPixels/(width*height)*100).toFixed(3);
         console.log("Comparison complete and an image has been created.");
+        if(percentDiff>testThreshhold){
+            console.log("--- Test FAILED --- Difference above "+testThreshold+"% threshold @ "+percentDiff+"%.")
+        }else{
+            console.log("--- Test PASSED --- Difference below "+testThreshold+"% threshold @ "+percentDiff+"%.")
+        }
+
+        
     }
 };
