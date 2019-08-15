94f7f2d784b863be8ce841b79884f59cf9f70a6b
tom8825 <tlparker1988@gmail.com>
2019-08-15 08:49:01 +0100

    added express and react

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/customers', (req, res) => {
+  const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+  ];
+
+  res.json(customers);
+});
+
+app.post('/api/post', function (req, res) {
+    res.send('Got a POST request')
+})
+
+const port = 5000;
+
+app.listen(port, () => `Server running on port ${port}`);
